edf648e4041ed8d9e1b9f4289e0792c56cc11f31
Oleg Volkov <OGVolkov@gmail.com>
2018-12-30 00:45:10 +0100

    IE11 support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'react-app-polyfill/ie11';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
