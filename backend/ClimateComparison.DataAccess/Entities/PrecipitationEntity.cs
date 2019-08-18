4c65f1e9fb3b7a4e141119822e8b0aeffc54b3ad
Oleg Volkov <OGVolkov@gmail.com>
2019-08-18 11:55:26 +0200

    Get precipitation from table storage

---
+++ backend/ClimateComparison.DataAccess/Entities/PrecipitationEntity.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ClimateComparison.DataAccess.Entities
+{
+    public class PrecipitationEntity : TableEntity
+    {
+        public double Average { get; set; }
+    }
+}
