b12d58e4902207a007efb1e21ad519e6e5a1f9f6
Oleg Volkov <OGVolkov@gmail.com>
2019-09-09 19:21:43 +0200

    Migrate cities to save the ids

---
+++ tools/import/migrate-cities-tstorage/migrate-cities-tstorage/migrate-cities/PlaceRow.cs
@@ -0,0 +1,19 @@
+﻿namespace ClimateComparison.Migrate.Cities
+{
+    public class PlaceRow
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AltNames { get; set; }
+
+        public float Longitude { get; set; }
+
+        public float Latitude { get; set; }
+
+        public int Population { get; set; }
+
+        public string CountryCode { get; set; }
+    }
+}
