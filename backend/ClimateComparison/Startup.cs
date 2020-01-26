16c149dfd0ac668c0e3d112c69f978975c5987b3
Oleg Volkov <OGVolkov@gmail.com>
2020-01-26 14:14:02 +0100

    AppInsights

--- backend/ClimateComparison/Startup.cs
+++ backend/ClimateComparison/Startup.cs
@@ -12,8 +12,9 @@ namespace ClimateComparison
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddApplicationInsightsTelemetry();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSingleton<PlaceRepository>();
             services.AddSingleton<ClimateRepository>();
