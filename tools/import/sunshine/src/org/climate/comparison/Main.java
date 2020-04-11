b6a54748f23a01100ec3274867da6c8bfd876e02
Oleg Volkov <OGVolkov@gmail.com>
2020-04-11 16:20:14 +0200

    Loop through places in a text file

--- tools/import/sunshine/src/org/climate/comparison/Main.java
+++ tools/import/sunshine/src/org/climate/comparison/Main.java
@@ -3,6 +3,8 @@ package org.climate.comparison;
 import com.microsoft.azure.storage.StorageException;
 import ucar.ma2.InvalidRangeException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
@@ -13,13 +15,23 @@ public class Main {
         TableStorageService tableStorage = new TableStorageService(storageConnectionString, "radiation");
 
         try (Cdf cdf = new Cdf(args[0])) {
-            int placeId = 105989;
-            double[] result = cdf.get(52.15, 5.38);
+            try (BufferedReader placesFile = new BufferedReader(new FileReader(args[1]))) {
+                String line;
+                while ((line = placesFile.readLine()) != null) {
+                    String[] fields = line.split("\\s+");
 
-            for (int i = 0; i < 12; i++) {
-                tableStorage.save(placeId, i + 1, result[i]);
+                    int placeId = Integer.parseInt(fields[0]);
+                    double lat = Double.parseDouble(fields[1]);
+                    double lon = Double.parseDouble(fields[2]);
+
+                    double[] result = cdf.get(lat, lon);
+
+                    for (int i = 0; i < 12; i++) {
+                        tableStorage.save(placeId, i + 1, result[i]);
+                    }
+                    System.out.println("Imported " + placeId);
+                }
             }
-            System.out.println("Imported " + placeId);
         }
     }
 }
