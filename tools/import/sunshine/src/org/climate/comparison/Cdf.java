0663d146271f00103b0e4466fe171e5f8feb14bb
Oleg Volkov <OGVolkov@gmail.com>
2020-04-11 17:14:49 +0200

    Fix coordinates

--- tools/import/sunshine/src/org/climate/comparison/Cdf.java
+++ tools/import/sunshine/src/org/climate/comparison/Cdf.java
@@ -31,11 +31,14 @@ public class Cdf implements AutoCloseable {
         offset = radiation.findAttribute("add_offset").getNumericValue().doubleValue();
     }
 
+    // lat: [-90, 90]
+    // lon: (-180, 180]
     public double[] get(double lat, double lon) throws IOException, InvalidRangeException {
         double[] result = new double[12];
 
-        int r = 1 + (int)Math.round((90 - lat) * latitudeGranularity / 180);
-        int c = 1 + (int)Math.round(lon * longitudeGranularity / 360);
+        int r = 1 + (int)Math.round((90 - lat) * (latitudeGranularity-1) / 180);
+        if (lon < 0) lon += 360;
+        int c = 1 + (int)Math.round(lon * (longitudeGranularity-1) / 360);
 
         int[] origin = new int [] { 0, r, c };
         int[] size = new int [] { 12, 1, 1 };
