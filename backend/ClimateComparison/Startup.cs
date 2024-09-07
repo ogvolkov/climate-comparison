0d69136f9fa862139b6769a3673303798f47f426
Oleg Volkov <OGVolkov@gmail.com>
2024-09-07 17:28:05 +0100

    Update general packages

--- backend/ClimateComparison/Startup.cs
+++ backend/ClimateComparison/Startup.cs
@@ -4,11 +4,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using ClimateComparison.DataAccess.Repositories;
+using Microsoft.Extensions.Configuration;
 
 namespace ClimateComparison
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
